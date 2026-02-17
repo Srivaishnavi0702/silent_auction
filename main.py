b1e9c244a92b9e369c8012a2a63098a91220b710
Srivaishnavi0702 <anbusrivaishnavi@gmail.com>
2026-02-17 22:41:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-# TODO-1: Ask the user for input
-# TODO-2: Save data into dictionary {name: price}
-# TODO-3: Whether if new bids need to be added
-# TODO-4: Compare bids in dictionary
 #importing the logo
 from art import logo
 print(logo)
+
 #user input
 auction = {}
 while True:
@@ -15,6 +12,7 @@ while True:
     if next_bidder == "no":
         break
     print("\n" * 100)
+
 #Calculate the highest bid
 winner = ""
 winner_cost = 0
@@ -22,5 +20,6 @@ for key in auction:
     if auction[key] > winner_cost:
         winner_cost = auction[key]
         winner = key
+        
 #Displays the winner
 print(f"The winner is {winner} with a bid of ${winner_cost}")
